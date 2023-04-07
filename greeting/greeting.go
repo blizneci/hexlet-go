3b6402ff8d431637f8cac665598a9ee09ce13edd
Kirill Zhelvakov <blzneci.89@gmail.com>
2023-04-07 18:07:58 +0300

    Not finished

--- greeting/greeting.go
+++ greeting/greeting.go
@@ -1,10 +1,22 @@
+// Package greeting implements a Hello function to return string to the main.
 package greeting
 
-var greeting string = "Golang for Brave!"
+import "fmt"
 
-func Hello() string {
-    return greeting
+const (
+	greeting    = "Golang for Brave!"
+	description = `Это приложение раскрашивает введенный текст в указанные цвета.
+Поддерживается изменение цвета текста, цвета фона текста и дополнительные аттрибуты.`
+)
+
+// Hello function returns string to the main function
+func SayHello() {
+	fmt.Println(greeting)
+}
+
+// Prints description
+func Introduce() {
+	fmt.Println(description)
 }
 
 //
-
--- main.go
+++ main.go
@@ -2,44 +2,114 @@
 package main
 
 import (
-	"errors"
+	"bufio"
 	"fmt"
+	"os"
+	"time"
 
 	"github.com/fatih/color"
 	"github.com/hexlet-go/greeting"
-	"github.com/sirupsen/logrus"
 )
 
+const instruction = `Это приложение для вывода текста, раскрашенного в разные цвета.
+Можно указать цвет текста, фона и задать дополнительные аттрибуты.
+
+`
+
+colors := map[string]color.Attirbute{
+	"белый": color.FgWhite,
+	"красный": color.FgRed,
+	"зеленый": color.FgGreen,
+	"желтый": color.FgYellow,
+	"синий": color.FgBlue,
+	"пурпурный": color.FgMagenta,
+	"голубой": color.FgCyan,
+}
+
+backgrounds := map[string]color.Attirbute{
+	"черный": color.BgBlack,
+	"красный": color.BgRed,
+	"зеленый": color.BgGreen,
+	"желтый": color.BgYellow,
+	"синий": color.BgBlue,
+	"пурпурный": color.BgMagenta,
+	"голубой": color.BgCyan,
+	"белый": color.BgWhite,
+}
+
+attributes := map[string]color.Attribute{
+	"жирный": color.Bold,
+	"тусклый": color.Faint,
+	"курсив": color.Italic,
+	"нижнее подчеркивание": color.Underline,
+	"мигающий медленно": color.BlinkSlow,
+	"мигающий быстро": color.BlinkRapid,
+	"обратное видео": color.ReverseVideo,
+	"скрытый": color.Concealed,
+	"зачеркнутый": color.CrossedOut,
+}
+
+type Text struct {
+	text    string
+	fgColor []string
+	bgColor []string
+	attr    []string
+}
+
+func exit() {
+	fmt.Println("Работа приложения завершается.")
+	time.Sleep(1 * time.Second)
+}
+
+func inputText(r *bufio.Reader) string {
+	var buf []byte
+	buf, _ = r.ReadSlice('\n')
+	return string(buf[:len(buf)-1])
+}
+func getText(r *Reader) Text, error {
+	t := Text{}
+	fmt.Println("Введите текст:")
+	t.text = inputText(r)
+	if t.text == "q" {
+		err := errors.New("")
+		return Text{}, 
+	}
+	
+}
+
 func main() {
-	fmt.Println(greeting.Hello())
-	logrus.Println("Hello, Hexlet!")
-	color.Cyan("Prints text in cyan.")
-	// Create a new color object
-	c := color.New(color.FgCyan).Add(color.Underline)
-	c.Println("Prints cyan text with an underline.")
+	greeting.SayHello()
+	greeting.Introduce()
+	r := bufio.NewReader(os.Stdin)
 
-	// Or just add them to New()
-	d := color.New(color.FgCyan, color.Bold)
-	d.Printf("This prints bold cyan %s\n", "too!.")
+	fmt.Printf("Введенный текст: %s\n", t.text)
+	fmt.Printf("Введенный цвет текста: %s\n", t.fgColor)
+	fmt.Printf("Введенные цвет фона: %s\n", t.bgColor)
+	fmt.Printf("Введеный аттрибут: %s\n", t.attr)
 
-	// Mix up foreground and background colors, create new mixes!
-	red := color.New(color.FgRed)
-	err := errors.New("some critical error")
-	red.Println("Warning")
-	red.Printf("Error: %s\n", err)
 
-	boldRed := red.Add(color.Bold)
-	boldRed.Println("This will print text in bold red.")
+	/*
+		fmt.Println("Text with colors")
+		for _, col := range colors {
+			c := color.New(col)
+			c.Printf("text\n")
+		}
 
-	whiteBackground := red.Add(color.BgWhite)
-	whiteBackground.Println("Red text with white background.")
+		fmt.Println("Text with background")
+		for _, bg := range backgrounds {
+			b := color.New(bg)
+			b.Print("text")
+			fmt.Println()
+		}
 
-	color.Set(color.FgYellow)
-
-	fmt.Println(greeting.Hello())
-	logrus.Println("Hello, Hexlet!")
-
-	color.Unset()
+		fmt.Println("Text with attributes")
+		for _, attr := range attributes {
+			a := color.New(attr)
+			a.Print("text")
+			fmt.Println()
+		}
+		color.New().Add(color.Underline).Println("Bold text")
+	*/
 
 }
 
